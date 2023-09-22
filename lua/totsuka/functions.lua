35439b9ba66c6af0e9f57ef311a7a3468fbe8706
tsukki9696 <viniciusdesouzagvitorino@gmail.com>
2023-09-22 14:08:51 -0300

    feat: added functions.lua

---
+++ lua/totsuka/functions.lua
@@ -0,0 +1,12 @@
+local c = vim.cmd
+
+-- Terminals don't have numbers or relative numbers
+c([[autocmd TermOpen * setlocal nonumber norelativenumber]])
+
+-- Highlight on yank
+c[[
+augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=120}
+augroup END
+]]
